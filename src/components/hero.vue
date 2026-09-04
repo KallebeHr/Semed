4546768c65f49fdcbf3306b17f2edd40ce03e4e3
KallebeHr <kallebealmeidap2@gmail.com>
2026-09-04 10:31:49 -0300

    ss

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -2,7 +2,7 @@
   <section class="hero-main">
     <div class="p-container hero-grid">
       <div>
-        <p class="hero-eyebrow">EDUCAÇÃO QssUE CONECTA</p>
+        <p class="hero-eyebrow">Janyce tem verme na Barriga</p>
         <h1>
           {{
             configuracao.heroTitulo ||
